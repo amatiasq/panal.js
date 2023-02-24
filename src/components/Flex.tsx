dbe99a605881d9bac329af8c418e8d089015d30f
A. Matías Quezada <amatiasq@gmail.com>
2023-02-25 00:09:15 +0100

    cleanup

--- src/components/Flex.tsx
+++ src/components/Flex.tsx
@@ -1,7 +1,7 @@
 import { css } from '@emotion/css';
 import { Index } from 'solid-js';
 import { isPanelContent, PanelBranch, PanelGroup } from '../types';
-import { px, replaceItem, replaceItems } from '../utilities';
+import { px } from '../utilities';
 import { Divider } from './Divider';
 import { Panel } from './Panel';
 
@@ -28,18 +28,21 @@ export function Flex(props: {
   const content = () => props.content as PanelGroup;
 
   let dragging: {
-    index: number;
-    a: HTMLDivElement;
-    b: HTMLDivElement;
+    a: number;
+    b: number;
     aSize: number;
     bSize: number;
   } | null = null;
 
   function startResizeOperation(index: number) {
-    const a = panels[index - 1];
-    const b = panels[index];
     const prop = props.direction === 'row' ? 'offsetWidth' : 'offsetHeight';
-    dragging = { index, a, b, aSize: a[prop], bSize: b[prop] };
+
+    dragging = {
+      a: index - 1,
+      b: index,
+      aSize: panels[index - 1][prop],
+      bSize: panels[index][prop],
+    };
   }
 
   function endResizeOperation() {
@@ -51,24 +54,17 @@ export function Flex(props: {
       throw new Error('Resize operation not started');
     }
 
-    const { index, a, b, aSize, bSize } = dragging;
+    const { a, b, aSize, bSize } = dragging;
     const newASize = aSize + delta;
     const newBSize = bSize - delta;
 
-    a.style.flexBasis = px(newASize);
-    b.style.flexBasis = px(newBSize);
+    panels[a].style.flexBasis = px(newASize);
+    panels[b].style.flexBasis = px(newBSize);
 
-    const list = content().children;
-    props.onPanelsChange({
-      ...content(),
-      children: replaceItems(
-        list,
-        index - 1,
-        2,
-        { ...list[index - 1], size: newASize },
-        { ...list[index], size: newBSize }
-      ),
-    });
+    const copy = [...content().children];
+    copy[a] = { ...copy[a], size: newASize };
+    copy[b] = { ...copy[b], size: newBSize };
+    props.onPanelsChange({ ...content(), children: copy });
   }
 
   return (
@@ -96,12 +92,9 @@ export function Flex(props: {
               direction={otherDirection}
               content={item()}
               onPanelsChange={(updated) => {
-                console.log('Propagating panels change', updated);
-                const content = props.content as PanelGroup;
-                props.onPanelsChange({
-                  ...content,
-                  children: replaceItem(content.children, i, updated),
-                });
+                const copy = [...content().children];
+                copy[i] = updated;
+                props.onPanelsChange({ ...content(), children: copy });
               }}
             />
           </>
--- src/utilities.ts
+++ src/utilities.ts
@@ -1,20 +1,19 @@
-export function replaceItem<T>(list: T[], index: number, value: T) {
-  const before = list.slice(0, index);
-  const after = list.slice(index + 1);
-  return [...before, value, ...after];
-}
+// export function replaceItem<T>(list: T[], index: number, value: T) {
+//   const before = list.slice(0, index);
+//   const after = list.slice(index + 1);
+//   return [...before, value, ...after];
+// }
 
-export function replaceItems<T>(
-  list: T[],
-  index: number,
-  amount: number,
-  ...values: T[]
-) {
-  debugger;
-  const before = list.slice(0, index);
-  const after = list.slice(index + amount);
-  return [...before, ...values, ...after];
-}
+// export function replaceItems<T>(
+//   list: T[],
+//   index: number,
+//   amount: number,
+//   ...values: T[]
+// ) {
+//   const before = list.slice(0, index);
+//   const after = list.slice(index + amount);
+//   return [...before, ...values, ...after];
+// }
 
 export function px<T extends number | undefined>(value: T) {
   return value && `${value}px`;
