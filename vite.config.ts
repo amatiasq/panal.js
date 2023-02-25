41bacdeda5c5ec84fd03604c366582a63580732d
A. Matías Quezada <amatiasq@gmail.com>
2023-02-25 15:14:08 +0100

    fix github pages relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  base: '',
   server: { port: 3001 },
   build: { target: 'esnext' },
 });
